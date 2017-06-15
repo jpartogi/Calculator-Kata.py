a3ba8a8dcdae01817140ecbab0e5ef49064cf1ee
Joshua Partogi <jpartogi@live.com>
2017-06-15 10:08:43 +0800

    Add single number test case

--- tests/__init__.py
+++ tests/__init__.py
@@ -9,5 +9,10 @@ class CalculatorTest(unittest.TestCase):
     self.assertEqual(result, 0)
     return None
 
+  def testOneNumber(self):
+    calculator = Calculator()
+    result = calculator.add("1")
+    self.assertEqual(result, 1)
+
 if __name__ == '__main__':
     unittest.main()
